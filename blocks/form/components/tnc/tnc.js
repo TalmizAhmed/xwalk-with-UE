40206144df5707858f913c41088d0766d5963376
Talmiz Ahmed <talmiz.tech@gmail.com>
2024-08-13 11:09:33 +0530

    refactoring into class

--- blocks/form/components/tnc/tnc.js
+++ blocks/form/components/tnc/tnc.js
@@ -1,34 +1,54 @@
+// import { subscribe } from '../../rules/index.js';
+
 const textIntersectionClass = 'tnc__intersection';
 const textDecorationClass = 'tnc__text-decoration'
 
-export default async function decorate(fieldDiv, field, htmlForm) {
-    console.log(fieldDiv, field, htmlForm);
-    const textWrapper = fieldDiv.querySelector('.plain-text-wrapper');
-    textWrapper.classList.add(textDecorationClass);
-    const intersection = document.createElement('div');
-    intersection.classList.add(textIntersectionClass);
-    textWrapper.appendChild(intersection);
-    handleScroll(fieldDiv);
-}
-
-function handleScroll(field) {
-    const intersection = field.querySelector(textIntersectionClass);
-    if (intersection) {
-        console.log(intersection);
-        // const self = this;
-        // const io = new IntersectionObserver(onIntersection, {
-        //     threshold: [1],
-        // })
-        // function onIntersection ([{isIntersecting}]) {
-        //     const isEnabled = self.getModel()?.enabled && !self.getModel()?.readOnly;
-        //     if (isIntersecting) {
-        //         if (isEnabled) {
-        //             self.children.filter(c => c.getModel()._jsonModel.fieldType === 'checkbox').forEach(cb => {
-        //                 cb.getModel().enabled = true;
-        //             })}
-        //         io.unobserve(intersection);
-        //     }
-        // }
-        // io.observe(intersection)
+class TermsAndConditions {
+    constructor(fieldDiv) {
+        this.fieldDiv = fieldDiv;
+        this.formModel = null;
+        this.decorate();
     }
+
+    setFormModel(model) {
+        this.formModel = model;
+    }
+
+    decorate() {
+        const textWrapper = this.fieldDiv.querySelector('.plain-text-wrapper');
+        textWrapper.classList.add(textDecorationClass);
+        const intersection = document.createElement('div');
+        intersection.classList.add(textIntersectionClass);
+        textWrapper.appendChild(intersection);
+        this.handleScroll();
+    }
+
+    handleScroll() {
+        const intersection = this.fieldDiv.querySelector(textIntersectionClass);
+        if (intersection) {
+            console.log(intersection);
+            // const self = this;
+            // const io = new IntersectionObserver(onIntersection, {
+            //     threshold: [1],
+            // })
+            // function onIntersection ([{isIntersecting}]) {
+            //     const isEnabled = self.getModel()?.enabled && !self.getModel()?.readOnly;
+            //     if (isIntersecting) {
+            //         if (isEnabled) {
+            //             self.children.filter(c => c.getModel()._jsonModel.fieldType === 'checkbox').forEach(cb => {
+            //                 cb.getModel().enabled = true;
+            //             })}
+            //         io.unobserve(intersection);
+            //     }
+            // }
+            // io.observe(intersection)
+        }
+    }
+}
+export default async function decorate(tncDiv, field, htmlForm) {
+    const tnc = new TermsAndConditions(tncDiv);
+    // subscribe(tncDiv, async (fieldDiv, formModel) => {
+    //     tnc.setFormModel(formModel);
+    // })
+    return tncDiv;
 }
