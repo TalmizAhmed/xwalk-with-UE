37752b5396e86a10fb3a6e3a9fe40c1d545ece41
Talmiz Ahmed <talmiz.tech@gmail.com>
2024-08-13 10:38:36 +0530

    bug

--- blocks/form/components/tnc/tnc.js
+++ blocks/form/components/tnc/tnc.js
@@ -3,16 +3,16 @@ const textDecorationClass = 'tnc__text-decoration'
 
 export default async function decorate(fieldDiv, field, htmlForm) {
     console.log(fieldDiv, field, htmlForm);
-    const textWrapper = document.querySelector('.plain-text-wrapper');
+    const textWrapper = fieldDiv.querySelector('.plain-text-wrapper');
     textWrapper.classList.add(textDecorationClass);
-    const intersection = document.createElement('div');
+    const intersection = fieldDiv.createElement('div');
     intersection.classList.add(textIntersectionClass);
     textWrapper.appendChild(intersection);
-    handleScroll();
+    handleScroll(fieldDiv);
 }
 
-function handleScroll() {
-    const intersection = document.querySelector(textIntersectionClass);
+function handleScroll(field) {
+    const intersection = field.querySelector(textIntersectionClass);
     if (intersection) {
         console.log(intersection);
         // const self = this;
