876d6d957425de164ec9d1fe08977bf020965db8
Talmiz Ahmed <talmiz.tech@gmail.com>
2024-09-02 12:12:19 +0530

    commenting subscribe

--- blocks/form/components/tnc/tnc.js
+++ blocks/form/components/tnc/tnc.js
@@ -1,4 +1,4 @@
-import {subscribe} from "../../rules";
+// import {subscribe} from "../../rules";
 
 const textIntersectionClass = 'tnc__intersection';
 const textDecorationClass = 'tnc__text-decoration';
