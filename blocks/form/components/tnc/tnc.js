f496117c22a6ff5daaa56c7a8b7a98423f09b5b9
Talmiz Ahmed <talmiz.tech@gmail.com>
2024-09-02 11:59:18 +0530

    Revert "tnc io impl"
    This reverts commit b4bb9387d631c9acd7aa5c4c7a51b67ceb915abe.

--- blocks/form/components/tnc/tnc.js
+++ blocks/form/components/tnc/tnc.js
@@ -1,4 +1,4 @@
-import {subscribe} from "../../rules";
+// import { subscribe } from '../../rules/index.js';
 
 const textIntersectionClass = 'tnc__intersection';
 const textDecorationClass = 'tnc__text-decoration';
@@ -32,26 +32,29 @@ class TermsAndConditions {
     const intersection = this.fieldDiv.querySelector(textIntersectionClass);
     if (intersection) {
       console.log(intersection);
-      const io = new IntersectionObserver(onIntersection, {
-          threshold: [1],
-      })
-      function onIntersection ([{isIntersecting}]) {
-          const isEnabled = this.formModel?.enabled && !this.formModel?.readOnly;
-          if (isIntersecting) {
-              if (isEnabled) {
-                  console.log('do something')
-              }
-              io.unobserve(intersection);
-          }
-      }
-      io.observe(intersection)
+      // const self = this;
+      // const io = new IntersectionObserver(onIntersection, {
+      //     threshold: [1],
+      // })
+      // function onIntersection ([{isIntersecting}]) {
+      //     const isEnabled = self.getModel()?.enabled && !self.getModel()?.readOnly;
+      //     if (isIntersecting) {
+      //         if (isEnabled) {
+      //             self.children.filter(c =>
+      //                c.getModel()._jsonModel.fieldType === 'checkbox').forEach(cb => {
+      //                   cb.getModel().enabled = true;
+      //             })}
+      //         io.unobserve(intersection);
+      //     }
+      // }
+      // io.observe(intersection)
     }
   }
 }
 export default async function decorate(tncDiv, fieldJson) {
   const tnc = new TermsAndConditions(tncDiv, fieldJson);
-  subscribe(tncDiv, async (fieldDiv, formModel) => {
-      tnc.setFormModel(formModel);
-  })
+  // subscribe(tncDiv, async (fieldDiv, formModel) => {
+  //     tnc.setFormModel(formModel);
+  // })
   return tnc.getfieldDiv();
 }
