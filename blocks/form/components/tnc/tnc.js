5832ad46c2690873f00abbe7efad7b9af003e47a
Talmiz Ahmed <talmiz.tech@gmail.com>
2024-09-09 11:25:13 +0530

    Revert "updated tnc js"
    This reverts commit 486378ec2789509e3cb224311e8858e9bc4f04b9.

--- blocks/form/components/tnc/tnc.js
+++ blocks/form/components/tnc/tnc.js
@@ -1,3 +1,5 @@
+import { subscribe } from '../../rules/index.js';
+
 const textIntersectionClass = 'tnc__text-intersect';
 const textDecorationClass = 'tnc__text-decoration';
 
@@ -27,24 +29,27 @@ class TermsAndConditions {
   }
 
   handleScroll() {
-    const intersection = this.fieldDiv.querySelector(`.${textIntersectionClass}`);
+    const intersection = this.fieldDiv.querySelector('.' + textIntersectionClass);
     if (intersection) {
-      const io = new IntersectionObserver(([{ isIntersecting }]) => {
-        if (isIntersecting) {
+      const io = new IntersectionObserver(onIntersection, {
+          threshold: [1],
+      })
+      function onIntersection ([{isIntersecting}]) {
+        if(isIntersecting) {
           /*
-          * TODO: Enable the checkboxes that are disabled by default via the model.
-          *  Currently they are enabled by default
+          * TODO: Enable the checkboxes that are disabled by default via the model. Currently they are enabled by default
           * */
           io.unobserve(intersection);
         }
-      }, {
-        threshold: [1],
-      });
+      }
       io.observe(intersection);
     }
   }
 }
 export default async function decorate(tncDiv, fieldJson) {
   const tnc = new TermsAndConditions(tncDiv, fieldJson);
+  subscribe(tncDiv, async (fieldDiv, formModel) => {
+      tnc.setFormModel(formModel);
+  })
   return tnc.getfieldDiv();
 }
